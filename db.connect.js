8aff09e728146bf78a970522889ff3408c6df13c
aakash <37771235+aakashindoriya@users.noreply.github.com>
2023-01-25 21:38:05 +0530

    dotenv

--- db.connect.js
+++ db.connect.js
@@ -1,5 +1,5 @@
 let mongoose=require("mongoose")
 let connect=async()=>{
-    return mongoose.connect(process.env.url)
+    return mongoose.connect("mongodb+srv://aakashindoriya:aakash123@cluster0.tbtucsa.mongodb.net/?retryWrites=true&w=majority")
 }
 module.exports=connect
